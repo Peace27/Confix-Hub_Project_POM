6446c685200139424c37458f486e1631aaf4ebe6
Sonika01$ <peacecornell1@gmail.com>
2024-10-10 14:58:11 +0100

    Package update

--- test_suite.py
+++ test_suite.py
@@ -1,6 +1,9 @@
 import pytest
+
 from selenium import webdriver
 from selenium.webdriver.chrome.options import Options
+# from selenium.webdriver.edge.options import Options
+# from selenium.webdriver.firefox.options import Options
 
 
 from ActionPage.ActionsPage import InvalidLoginActionsPage, ValidLoginActionsPage, ClickAddNewContact1ActionsPage, \
@@ -18,8 +21,8 @@ from Config.config import Config
 def driver_setup():
     chrome_options = Options()
     # Uncomment the line below to run in headless mode
-    chrome_options.add_argument("--headless")  # Run Chrome in headless mode
-    chrome_options.add_argument("--disable-gpu")  # Optional: Disable GPU acceleration
+    # chrome_options.add_argument("--headless")  # Run Chrome in headless mode
+    # chrome_options.add_argument("--disable-gpu")  # Optional: Disable GPU acceleration
     driver = webdriver.Chrome(options=chrome_options)
     driver.implicitly_wait(20)
     driver.maximize_window()
@@ -268,3 +271,4 @@ def test_Add_New_Contact10_Details(login1):
 def test_click_logout_button(login1):
     clicklogout = ClickLogoutButtonActionPage(login1.driver)
     clicklogout.click_logout_button()
+
