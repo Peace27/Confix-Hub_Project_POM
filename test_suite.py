6e348da8346592e7320f8e4a7a66d32917ee9acc
Sonika01$ <peacecornell1@gmail.com>
2024-10-11 12:43:58 +0100

    new2

--- test_suite.py
+++ test_suite.py
@@ -17,8 +17,8 @@ from Config.config import Config
 def driver_setup():
     chrome_options = Options()
     # Uncomment the line below to run in headless mode
-    # chrome_options.add_argument("--headless")  # Run Edge in headless mode
-    # chrome_options.add_argument("--disable-gpu")  # Optional: Disable GPU acceleration
+    chrome_options.add_argument("--headless")  # Run Edge in headless mode
+    chrome_options.add_argument("--disable-gpu")  # Optional: Disable GPU acceleration
     driver = webdriver.Edge(options=chrome_options)
     driver.implicitly_wait(20)
     driver.maximize_window()
